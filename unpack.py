e18325e3558a1c5e82453bbcc5ec512feda2f766
buket <buketzeren354@gmail.com>
2022-02-28 13:34:01 +0300

    added unpack function

--- unpack.py
+++ unpack.py
@@ -1,5 +1,6 @@
 #BUKET ZEREN, EMİRHAN YILMAZ GÜNEY
 import pandas
+from bitstring import BitArray
 
 def find_and_load_table(packed_data):
     '''
@@ -17,20 +18,31 @@ def find_and_load_table(packed_data):
 def decode(packed_data,struct_table):
     cursor=0
     decoded_data=[]
+    print(packed_data)
     for i in range(len(struct_table)):
         #read the row
         d_type=struct_table.loc[i,"type"]
         bit_count=struct_table.loc[i,"bit_count"]
-        data_to_decode=packed_data[cursor:cursor+bit_count]
-
-        if d_type=="str":
-            pass
-        elif d_type=="float":
-            pass
-        elif d_type=="int":
-            pass
         
+        #get the related part 
+        data_to_decode=packed_data[cursor:cursor+bit_count]
+        cursor+=bit_count
+        
+        if d_type=="str":
+            binary_chars=[data_to_decode[i*8:(i+1)*8] for i in range(bit_count//8)]
+            print(binary_chars)
+            char_list=[chr(int(i,2)) for i in binary_chars]
+            print(char_list)
+            result="".join(char_list)
+            print(result)
+        elif d_type=="float":
+            result=BitArray(float=data_to_decode,length=bit_count).float
+        elif d_type=="int":
+            result=int(data_to_decode,2)
+        
+        decoded_data.append(result)
+    return decoded_data
 
-incoming_data="00000001"+"00000001"
+incoming_data="00000001"+"00000001"+"01000010"+"01011010"+"00011100"+"00000010"
 table=find_and_load_table(incoming_data)
-decode(table)
+print(decode(incoming_data[16:],table))
